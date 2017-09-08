fc1d95b1a19d0e45b39f07a24435cb1569218e3c
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-09-08 10:25:34 -0700

    added embed to links

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,41 +2,42 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Lesson.create([
-  # {
-  #   name: 'Introduction To JavaScript',
-  #   url: 'https://www.youtube.com/watch?v=T93WOuwUZ3s',
-  #   difficulty: 'Beginner',
-  #   author: 'NotesAndSoft',
-  #   language: 'JavaScript',
-  #   date: 'Sep 7, 2017'
-  # },
-  # {
-  #   name: 'Arrow Functions',
-  #   url: 'https://www.youtube.com/watch?v=rfsWnO980H8',
-  #   difficulty: 'Advanced',
-  #   author: 'Chris Hawkes',
-  #   language: 'JavaScript',
-  #   date: 'Aug 13, 2016'
-  # },
-  #
-  #
-  #
-  # ])
-#
-#   Playlist.create([
-#     {
-#       name: 'JavaScript for Beginners',
-#       difficulty: 'Beginner',
-#       number: 1,
-#       languages: 'JavaScript'
-#     },
-#
-#
-#
-#
-#     ])
+Lesson.create([
+  {
+    name: 'Introduction To JavaScript',
+    url: 'https://www.youtube.com/embed/T93WOuwUZ3s',
+    difficulty: 'Beginner',
+    author: 'NotesAndSoft',
+    language: 'JavaScript',
+    date: 'Sep 7, 2017'
+  },
+  {
+    name: 'Arrow Functions',
+    url: 'https://www.youtube.com/embed/rfsWnO980H8',
+    difficulty: 'Advanced',
+    author: 'Chris Hawkes',
+    language: 'JavaScript',
+    date: 'Aug 13, 2016'
+  },
+
+
+
+  ])
+
+  Playlist.create([
+    {
+      name: 'JavaScript for Beginners',
+      difficulty: 'Beginner',
+      number: 1,
+      languages: 'JavaScript'
+    },
+
+
+
+
+    ])
