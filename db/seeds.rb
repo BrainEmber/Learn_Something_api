fc35448c8335d15146115fa78daf9ca0f933c8c8
Jellis <venetianself@hotmail.com>
2017-09-09 15:39:53 -0500

    seeded more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,38 +7,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Lesson.create([
+#   {
+#     name: 'Introduction To JavaScript',
+#     url: 'https://www.youtube.com/watch?v=T93WOuwUZ3s',
+#     difficulty: 'Beginner',
+#     author: 'NotesAndSoft',
+#     language: 'JavaScript',
+#     date: 'Sep 7, 2017'
+#   },
+#   {
+#     name: 'Arrow Functions',
+#     url: 'https://www.youtube.com/watch?v=rfsWnO980H8',
+#     difficulty: 'Advanced',
+#     author: 'Chris Hawkes',
+#     language: 'JavaScript',
+#     date: 'Aug 13, 2016'
+#   },
+
+
+
+#   ])
+
+#   Playlist.create([
+#     {
+#       name: 'JavaScript for Beginners',
+#       difficulty: 'Beginner',
+#       number: 1,
+#       languages: 'JavaScript'
+#     },
+
+
+
+
+#     ])
+
+
 Lesson.create([
   {
-    name: 'Introduction To JavaScript',
-    url: 'https://www.youtube.com/watch?v=T93WOuwUZ3s',
-    difficulty: 'Beginner',
-    author: 'NotesAndSoft',
-    language: 'JavaScript',
-    date: 'Sep 7, 2017'
+    name: 'Introduction To Python',
+    url: 'https://www.youtube.com/watch?v=YSe9Tu_iNQQ',
+    difficulty: 'Intermediate',
+    author: 'sentdex',
+    language: 'Python',
+    date: 'Nov 1, 2016'
   },
   {
-    name: 'Arrow Functions',
-    url: 'https://www.youtube.com/watch?v=rfsWnO980H8',
+    name: 'Methods as First-class Functions',
+    url: 'https://www.youtube.com/watch?v=aIcqAc9S69Q',
     difficulty: 'Advanced',
-    author: 'Chris Hawkes',
-    language: 'JavaScript',
-    date: 'Aug 13, 2016'
+    author: 'Frank Bonetti',
+    language: 'Ruby',
+    date: 'June 15, 2014'
   },
 
 
 
   ])
-
-  Playlist.create([
-    {
-      name: 'JavaScript for Beginners',
-      difficulty: 'Beginner',
-      number: 1,
-      languages: 'JavaScript'
-    },
-
-
-
-
-    ])
-
