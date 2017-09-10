be0e2595ff3620e96ac7c1930727e7ec7b82644f
Jellis <venetianself@hotmail.com>
2017-09-10 00:18:59 -0500

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Lesson.create([
-#   {
-#     name: 'Introduction To JavaScript',
-#     url: 'https://www.youtube.com/watch?v=T93WOuwUZ3s',
-#     difficulty: 'Beginner',
-#     author: 'NotesAndSoft',
-#     language: 'JavaScript',
-#     date: 'Sep 7, 2017'
-#   },
-#   {
-#     name: 'Arrow Functions',
-#     url: 'https://www.youtube.com/watch?v=rfsWnO980H8',
-#     difficulty: 'Advanced',
-#     author: 'Chris Hawkes',
-#     language: 'JavaScript',
-#     date: 'Aug 13, 2016'
-#   },
+Lesson.create([
+  {
+    name: 'Introduction To JavaScript',
+    url: 'https://www.youtube.com/watch?v=T93WOuwUZ3s',
+    difficulty: 'Beginner',
+    author: 'NotesAndSoft',
+    language: 'JavaScript',
+    date: 'Sep 7, 2017'
+  },
+  {
+    name: 'Arrow Functions',
+    url: 'https://www.youtube.com/watch?v=rfsWnO980H8',
+    difficulty: 'Advanced',
+    author: 'Chris Hawkes',
+    language: 'JavaScript',
+    date: 'Aug 13, 2016'
+  },
 
 
 
-#   ])
+  ])
 
-#   Playlist.create([
-#     {
-#       name: 'JavaScript for Beginners',
-#       difficulty: 'Beginner',
-#       number: 1,
-#       languages: 'JavaScript'
-#     },
-
-
+  Playlist.create([
+    {
+      name: 'JavaScript for Beginners',
+      difficulty: 'Beginner',
+      number: 1,
+      languages: 'JavaScript'
+    },
 
 
-#     ])
+
+
+    ])
 
 
 Lesson.create([
@@ -60,7 +60,56 @@ Lesson.create([
     language: 'Ruby',
     date: 'June 15, 2014'
   },
+])
+Lesson.create([
+  {
+    name: 'Ruby Class & Instance Methods',
+    url: 'https://www.youtube.com/watch?v=i4uiyWA8eFk',
+    difficulty: 'Intermediate',
+    author: 'schneems',
+    language: 'Ruby',
+    date: 'July 18, 2012'
+  },
+  {
+    name: 'Advanced Ruby on Rails',
+    url: 'https://www.youtube.com/watch?v=K0Y0yQNDrEc',
+    difficulty: 'Advanced',
+    author: 'Code School',
+    language: 'Ruby',
+    date: 'July 15, 2013'
+  },
 
-
+   {
+    name: 'Faker Gem Tutorial for Rails 5',
+    url: 'https://www.youtube.com/watch?v=5qQ2NLHrmTc',
+    difficulty: 'Beginner',
+    author: 'stevenjchang',
+    language: 'Ruby',
+    date: 'Jan 19, 2017'
+  },
+  {
+    name: 'Python Runthrough',
+    url: 'https://www.youtube.com/watch?v=N4mEzFDjqtA',
+    difficulty: 'Beginner',
+    author: 'Derek Banas',
+    language: 'Python',
+    date: 'November 10th, 2014'
+  },
+   {
+    name: 'Python Coroutines',
+    url: 'https://www.youtube.com/watch?v=7sCu4gEjH5I&list=PLTOBJKrkhpoMdsT9RUERSDdEVrViykAEQ',
+    difficulty: 'Advanced',
+    author: 'A. Jesse Jiryu Davis',
+    language: 'Python',
+    date: 'August 15th, 2015'
+  },
+  {
+    name: 'Python Coroutines',
+    url: 'https://www.youtube.com/watch?v=jGV8etzFF_w',
+    difficulty: 'Intermediate',
+    author: 'Code School',
+    language: 'Javascript',
+    date: 'April 20th, 2016'
+  },
 
   ])
